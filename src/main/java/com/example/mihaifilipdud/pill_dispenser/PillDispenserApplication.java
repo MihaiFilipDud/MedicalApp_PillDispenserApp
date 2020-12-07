72e0ebc66ddad2aca3f8048f3783a2b79db3f4fa
MihaiFilipDud <mihaifilipdud@gmail.com>
2020-12-07 18:18:46 +0200

    Initial commit

---
+++ src/main/java/com/example/mihaifilipdud/pill_dispenser/PillDispenserApplication.java
@@ -0,0 +1,13 @@
+package com.example.mihaifilipdud.pill_dispenser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillDispenserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PillDispenserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mihaifilipdud/pill_dispenser/PillDispenserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mihaifilipdud.pill_dispenser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillDispenserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
